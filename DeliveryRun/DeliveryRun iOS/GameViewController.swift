00d407dff28cc419de26c50ced4fabe61976d0a6
DavidShin <dnrur124@gmail.com>
2022-10-12 16:43:57 +0900

    [FEAT] GameViewController 코드 수정

--- DeliveryRun/DeliveryRun iOS/GameViewController.swift
+++ DeliveryRun/DeliveryRun iOS/GameViewController.swift
@@ -13,26 +13,30 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
+        // Sks 파일 불러오기 'GameScene.sks' as a GKScene.
         
-        // Present the scene
-        let skView = self.view as! SKView
-        skView.presentScene(scene)
-        
-        skView.ignoresSiblingOrder = true
-        skView.showsFPS = true
-        skView.showsNodeCount = true
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Root 노드 생성
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                // Set the scale mode to scale to fit the window 화면에 Scene파일 맞추기
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    
+                    // 모든 노드가 Z축 활성화
+                    view.ignoresSiblingOrder = false
+                    
+                    // Node갯수 및 Fps 보기
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
         }
     }
     
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
