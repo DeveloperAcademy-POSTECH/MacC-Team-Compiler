f7d4b2cb32df5b046c3d23984aa84bf4369c3695
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-10-12 18:00:37 +0900

    [TEST] 플레이어 이동 테스트

--- DeliveryRun/DeliveryRun Shared/GameScene.swift
+++ DeliveryRun/DeliveryRun Shared/GameScene.swift
@@ -12,7 +12,18 @@ class GameScene: SKScene {
     var player: SKNode?
     
     override func didMove(to view: SKView) {
-        player = childNode(withName: "player0")
+        player = childNode(withName: "player")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches { self.touchDown(atPoint:touch.location(in:self))}
+    }
+    
+    func touchDown(atPoint pos: CGPoint) {
+        let distance = CGPoint(x: pos.x, y:player!.position.y)
+        let movePlayer = SKAction.move(to: distance, duration: 0.5)
+
+        player!.run(SKAction.sequence([movePlayer]))
     }
 
 }
