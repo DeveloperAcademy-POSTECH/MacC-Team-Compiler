047d49969d8f2f33283dd5ee4011558609992396
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-10-12 15:40:51 +0900

    [DESIGN] 기본 색 및 폰트 추가

---
+++ DeliveryRun/DeliveryRun Shared/Style/Color.swift
@@ -0,0 +1,12 @@
+//
+//  Color.swift
+//  DeliveryRun iOS
+//
+//  Created by HWANG-C-K on 2022/10/12.
+//
+
+import UIKit
+
+extension UIColor {
+    static let deliveryrunBlack = UIColor(named:"deliveryrunBlack")
+}
