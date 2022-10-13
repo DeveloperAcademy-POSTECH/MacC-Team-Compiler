fa2d97e589368b1ff479ae925714dd5ba16e9442
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-10-13 17:08:47 +0900

    [TEST] accel, break 버튼 추가

--- DeliveryRun/DeliveryRun Shared/GameScene.swift
+++ DeliveryRun/DeliveryRun Shared/GameScene.swift
@@ -12,14 +12,40 @@ class GameScene: SKScene {
     var jumpButton: SKNode?
     var jumpKnob: SKNode?
     
+    var accelButton: SKNode?
+    var accelKnob: SKNode?
+    
+    var breakButton: SKNode?
+    var breakKnob: SKNode?
+    
     // Boolean
     var jumpAction = false
+    var accelAction = false
+    var breakAction = false
     
+    // Engine
+    var previousTimeInterval:TimeInterval = 0
+    let playerSpeed = 5
+    
+    // MARK: - Update
+    override func update(_ currentTime: TimeInterval) {
+        let deltaTime = currentTime - previousTimeInterval
+        previousTimeInterval = currentTime
+        player!.run(SKAction.moveTo(x: deltaTime * Double(playerSpeed) * 0.01, duration: currentTime))
+    }
     
     override func didMove(to view: SKView) {
         player = childNode(withName: "player")
         jumpButton = childNode(withName: "jumpButton")
         jumpKnob = jumpButton?.childNode(withName: "jumpKnob")
+        
+        accelButton = childNode(withName: "accelButton")
+        accelKnob = accelButton?.childNode(withName: "accelKnob")
+
+        
+        breakButton = childNode(withName: "breakButton")
+        breakKnob = breakButton?.childNode(withName: "breakKnob")
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -28,33 +54,54 @@ class GameScene: SKScene {
                 let location = touch.location(in: jumpButton!)
                 jumpAction = jumpKnob.frame.contains(location)
                 if jumpAction {
-                    self.jumpButtonPressed()
+                    doJump(atPoint: CGPoint(x:player!.position.x, y: player!.position.y + 50))
+                }
+            }
+            
+            if let accelKnob = accelKnob {
+                let location = touch.location(in: accelButton!)
+                accelAction = accelKnob.frame.contains(location)
+                if accelAction {
+                    doAccel()
+                }
+            }
+            
+            if let breakKnob = breakKnob {
+                let location = touch.location(in: breakButton!)
+                breakAction = breakKnob.frame.contains(location)
+                if breakAction {
+                    doBreak()
                 }
             }
             
             let location = touch.location(in: self)
-            if !(jumpButton?.contains(location))! {
-                self.noJumpButton()
+            if !(jumpButton!.contains(location) || accelButton!.contains(location) || breakButton!.contains(location)) {
                 self.touchDown(atPoint: location)
             }
         }
     }
-    
-//    // Player Position 이동
-//
+
+// MARK: - Action
     func touchDown(atPoint pos: CGPoint) {
         let distance = CGPoint(x: pos.x, y:player!.position.y)
         let movePlayer = SKAction.move(to: distance, duration: 0.5)
         
-
         player!.run(SKAction.sequence([movePlayer]))
     }
     
-    func jumpButtonPressed() {
+    func doJump(atPoint pos: CGPoint) {
+        let distance = CGPoint(x: player!.position.x, y:pos.y)
+        let movePlayer = SKAction.move(to: distance, duration: 0.1)
+        
+        player!.run(SKAction.sequence([movePlayer]))
         print("Jump")
     }
     
-    func noJumpButton() {
-        print("No")
+    func doAccel() {
+        print("Accel")
+    }
+    
+    func doBreak() {
+        print("Break")
     }
 }
