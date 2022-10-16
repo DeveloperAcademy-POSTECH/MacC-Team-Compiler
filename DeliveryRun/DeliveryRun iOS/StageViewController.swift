0646e421323f976d4708df18a53dc02930c82132
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-10-17 04:46:00 +0900

    [FEAT] CollectionView 추가

--- DeliveryRun/DeliveryRun iOS/StageViewController.swift
+++ DeliveryRun/DeliveryRun iOS/StageViewController.swift
@@ -7,11 +7,32 @@
 
 import UIKit
 
-class StageViewController: UIViewController {
+class StageViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "stagecell", for: indexPath) as?
+                UICollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension StageViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
