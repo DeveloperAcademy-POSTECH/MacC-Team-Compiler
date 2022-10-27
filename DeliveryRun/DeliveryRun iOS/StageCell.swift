fd986b86485692d0cf488e7db4b531d8924a9358
DavidShin <dnrur124@gmail.com>
2022-10-27 20:56:35 +0900

    # [TYPE] 내용 요약
    # 본문은 아래에 작성
    
    
    # --- COMMIT END ---
    # [TYPE] 리스트
    # ADD : 새로운 파일 추가
    # FEAT : 기능 추가 및 수정
    # DESIGN : 디자인 추가 및 수정
    # FIX : 버그 수정
    # DOCS : 문서 (README 등)
    # TEST : 테스트 코드
    # REFACTOR : 코드 리팩토링 (성능 개선)
    # COMMENT : 주석 추가
    # CHORE : 빌드 및 설정 파일 관리
    # ------------------
    #     타입 이름은 전체 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

---
+++ DeliveryRun/DeliveryRun iOS/StageCell.swift
@@ -0,0 +1,17 @@
+//
+//  StageCell.swift
+//  DeliveryRun iOS
+//
+//  Created by David_ADA on 2022/10/27.
+//
+
+import UIKit
+
+class StageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
