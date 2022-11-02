728f67a72551d24c7364f4e2d55dfc5c804230da
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-11-02 17:54:46 +0900

    [COMMENT] 주석 수정

--- DeliveryRun/DeliveryRun Shared/HUD.swift
+++ DeliveryRun/DeliveryRun Shared/HUD.swift
@@ -29,14 +29,14 @@ class PauseScreen:SKNode {
         
         isUserInteractionEnabled = true
         
-        // 일시정지 배경
+        // Pause Background
         pauseBase = SKShapeNode(rectOf: CGSize(width: 270, height: 135), cornerRadius: 10)
         pauseBase.fillColor = UIColor.deliveryrunBlack!.withAlphaComponent(0.6)
         pauseBase.zPosition = 20.0
         pauseBase.position = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
         addChild(pauseBase)
         
-        // 일시정지 창 제목
+        // Pause Title
         titleLabel = SKLabelNode(fontNamed: "BMJUAOTF")
         titleLabel.fontSize = 30
         titleLabel.text = "PAUSE"
@@ -45,19 +45,19 @@ class PauseScreen:SKNode {
         titleLabel.zPosition = 25.0
         addChild(titleLabel)
         
-        // 플레이 버튼
+        // Play Button
         playButton = SKSpriteNode(imageNamed: "Play")
         playButton.position = CGPoint(x: pauseBase.frame.midX - (pauseBase.frame.width / 3) + 5, y: pauseBase.frame.midY - 15)
         playButton.zPosition = 30.0
         addChild(playButton)
         
-        // 다시 시작 버튼
+        // Restart Button
         restartButton = SKSpriteNode(imageNamed: "Restart")
         restartButton.position = CGPoint(x: pauseBase.frame.midX, y: pauseBase.frame.midY - 15)
         restartButton.zPosition = 30.0
         addChild(restartButton)
         
-        // 홈 버튼
+        // Home Button
         homeButton = SKSpriteNode(imageNamed: "Home")
         homeButton.position = CGPoint(x: pauseBase.frame.midX + (pauseBase.frame.width / 3) - 5, y: pauseBase.frame.midY - 15)
         homeButton.zPosition = 30.0
