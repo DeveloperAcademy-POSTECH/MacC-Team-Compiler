be2ffae08e5cbab0dcc2f36f73cdee0d4ec9ffeb
DavidShin <dnrur124@gmail.com>
2022-12-05 00:31:35 +0900

    # [Garage] BackButton, SettingButton 구현
    # 본문은 아래에 작성
    
    
    # --- COMMIT END ---
    # [TYPE] 리스트
    # ADD : 새로운 파일 추가
    # FEAT : 기능 추가 및 수정
    # DESIGN : 디자인 추가 및 수정
    # FIX : 버그 수정
    # DOCS : 문서 (README 등)
    # TEST : 테스트 코드
    # REFACTOR : 코드 리팩토링 (성능 개선)
    # COMMENT : 주석 추가
    # CHORE : 빌드 및 설정 파일 관리
    # ------------------
    #     타입 이름은 전체 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- DeliveryRun/DeliveryRun iOS/Garage/GarageViewController.swift
+++ DeliveryRun/DeliveryRun iOS/Garage/GarageViewController.swift
@@ -10,7 +10,70 @@ import UIKit
 
 class GarageViewController: UIViewController {
     
+    @IBOutlet weak var backgroundView: UIImageView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var settingButton: UIButton!
+    
+    @IBOutlet weak var settingView: SettingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Background Blur
+        let image = UIImage(named: "RobbyBack")
+        backgroundView.image = image?.applyBlur_usingClamps(radius: 30)
+        
+        // Button
+        backButton.layer.shadowColor = UIColor.deliveryrunYellow?.cgColor
+        backButton.layer.shadowOpacity = 1
+        backButton.layer.shadowRadius = 20
+        backButton.layer.shadowPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: backButton.frame.width, height: backButton.frame.height)).cgPath
+        
+        settingButton.layer.shadowColor = UIColor.deliveryrunYellow?.cgColor
+        settingButton.layer.shadowOpacity = 1
+        settingButton.layer.shadowRadius = 20
+        settingButton.layer.shadowPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: settingButton.frame.width, height: settingButton.frame.height)).cgPath
+        
+        // SettingView
+        settingView.isHidden = true
+        settingView.layer.opacity = 1.0
+        
+    }
+    
+    
+    @IBAction func BackPressed(_ sender: UIButton) {
+        let robby = UIStoryboard.init(name: "Robby", bundle: nil)
+                guard let RobbyViewController = robby.instantiateViewController(withIdentifier: "RobbyViewController")as? RobbyViewController else {return}
+        RobbyViewController.modalPresentationStyle = .fullScreen
+                self.present(RobbyViewController, animated: false, completion: nil)
+        
+    }
+    @IBAction func SettingPressed(_ sender: UIButton) {
+        settingView.isHidden = false
+    }
+}
+
+
+// UIImage Blur
+
+extension UIImage {
+    
+    func applyBlur_usingClamps(radius: CGFloat) -> UIImage {
+        let context = CIContext()
+        guard let ciImage = CIImage(image: self), let clampFilter = CIFilter(name: "CIAffineClamp") , let blurFilter = CIFilter(name: "CIGaussianBlur") else {
+            return self
+        }
+        
+        clampFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        blurFilter.setValue(clampFilter.outputImage, forKey: kCIInputImageKey)
+        blurFilter.setValue(radius, forKey: kCIInputRadiusKey)
+        guard let output = blurFilter.outputImage,
+              let cgimg = context.createCGImage(output, from: ciImage.extent) else {
+            return self
+        }
+        return UIImage(cgImage: cgimg)
     }
 }
