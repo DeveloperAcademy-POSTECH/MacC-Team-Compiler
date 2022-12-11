ca6c64b8bb1774809ed87ed23d52f4cf5b99b749
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-12-11 20:13:43 +0900

    [FEAT] Stage 2 구성

---
+++ DeliveryRun/DeliveryRun Shared/Stages/Stage5.swift
@@ -0,0 +1,14 @@
+//
+//  Stage5.swift
+//  DeliveryRun iOS
+//
+//  Created by HWANG-C-K on 2022/12/11.
+//
+
+import SpriteKit
+import GameplayKit
+import AVFoundation
+
+class Stage5: GameScene {
+    override var endPoint: Double { return 13000.0 }
+}
