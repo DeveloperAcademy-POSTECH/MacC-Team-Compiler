0c2792638371de2d6866087f94d19276c5042e91
DavidShin <dnrur124@gmail.com>
2022-12-22 14:05:09 +0900

    # [TYPE] 내용 요약
    # 본문은 아래에 작성
    
    
    # --- COMMIT END ---
    # [TYPE] 리스트
    # ADD : 새로운 파일 추가
    # FEAT : 기능 추가 및 수정
    # DESIGN : 디자인 추가 및 수정
    # FIX : 버그 수정
    # DOCS : 문서 (README 등)
    # TEST : 테스트 코드
    # REFACTOR : 코드 리팩토링 (성능 개선)
    # COMMENT : 주석 추가
    # CHORE : 빌드 및 설정 파일 관리
    # ------------------
    #     타입 이름은 전체 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

---
+++ DeliveryRun/DeliveryRun Shared/Chapter/Chapter1/Chapter1.swift
@@ -0,0 +1,8 @@
+//
+//  Chapter1.swift
+//  DeliveryRun iOS
+//
+//  Created by David_ADA on 2022/12/22.
+//
+
+import Foundation
---
+++ DeliveryRun/DeliveryRun Shared/Chapter/Chapter2/Chapter2.swift
@@ -0,0 +1,40 @@
+//
+//  Stage1.swift
+//  DeliveryRun iOS
+//
+//  Created by HWANG-C-K on 2022/12/06.
+//
+
+import SpriteKit
+import GameplayKit
+import AVFoundation
+
+class Chapter2: GameScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+        super.didMove(to: view)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        super.touchesBegan(touches, with: event)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        super.touchesMoved(touches, with: event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        super.touchesEnded(touches, with: event)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        super.update(currentTime)
+    }
+    
+}
