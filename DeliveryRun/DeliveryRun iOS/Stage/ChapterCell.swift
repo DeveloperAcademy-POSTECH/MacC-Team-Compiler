a0fb0f5cbda5fce1c2316d45c58e0eda9bf15b64
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-12-24 04:11:34 +0900

    [FEAT] Chapter Cell 추가

---
+++ DeliveryRun/DeliveryRun iOS/Stage/ChapterCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChapterCell.swift
+//  DeliveryRun iOS
+//
+//  Created by HWANG-C-K on 2022/12/24.
+//
+
+import UIKit
+
+class ChapterCell: UICollectionViewCell {
+    static let id = "chaptercell"
+    
+}
