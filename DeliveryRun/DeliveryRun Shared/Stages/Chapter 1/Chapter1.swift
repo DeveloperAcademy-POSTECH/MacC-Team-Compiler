1f930174be429fe8e88d2e9ed239dbb6bc2a1460
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-12-28 19:58:18 +0900

    [FEAT] Chapter 1 Stage 파일 생성

--- DeliveryRun/DeliveryRun Shared/Stages/Chapter 1/Chapter1.swift
+++ DeliveryRun/DeliveryRun Shared/Stages/Chapter 1/Chapter1.swift
@@ -1,5 +1,5 @@
 //
-//  Stage1.swift
+//  Chapter1.swift
 //  DeliveryRun iOS
 //
 //  Created by HWANG-C-K on 2022/12/06.
@@ -9,6 +9,6 @@ import SpriteKit
 import GameplayKit
 import AVFoundation
 
-class Stage1: GameScene {
+class Chapter1: GameScene {
     override var endPoint: Double { return 13000.0 }
 }
