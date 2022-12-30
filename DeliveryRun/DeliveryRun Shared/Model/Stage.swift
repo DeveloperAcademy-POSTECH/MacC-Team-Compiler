9d7c4af6bc2948c7523df87dfe13feaea528b177
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2022-12-31 06:08:00 +0900

    [STYLE] 변수명 교체

--- DeliveryRun/DeliveryRun Shared/Model/Stage.swift
+++ DeliveryRun/DeliveryRun Shared/Model/Stage.swift
@@ -22,14 +22,14 @@ class Stage: Codable{
             return 0
         }
     }
-    var unLock: Bool
+    var isLock: Bool
     
-    init(name: String, image: String, targetRecord: Float, myRecord:Float,unLock: Bool) {
+    init(name: String, image: String, targetRecord: Float, myRecord: Float, isLock: Bool) {
         self.name = name
         self.image = image
         self.targetRecord = targetRecord
         self.myRecord = myRecord
-        self.unLock = unLock
+        self.isLock = isLock
     }
 }
 
